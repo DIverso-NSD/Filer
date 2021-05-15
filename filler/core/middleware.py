ab2232bbc44dcbcaf5392506e34b063a722dec58
kuderr <dakudryavcev@gmail.com>
2021-05-15 16:01:05 +0300

    jwt decode

--- filler/core/middleware.py
+++ filler/core/middleware.py
@@ -11,4 +11,7 @@ token_scheme = APIKeyHeader(name="Token", auto_error=False)
 async def verify_token(token: str = Depends(token_scheme)):
     print(token)
     print(settings.secret_key)
+
+    data = jwt.decode(token, settings.secret_key, algorithms=["HS256"])
+
     return User(id=1, login="kuder")
