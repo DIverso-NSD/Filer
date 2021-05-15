d66fc2b550b0e9da6a37f78a0234c1803be01cfc
kuderr <dakudryavcev@gmail.com>
2021-05-15 16:35:20 +0300

    refactor

--- filler/core/middleware.py
+++ filler/core/middleware.py
@@ -8,28 +8,18 @@ token_scheme = APIKeyHeader(name="Token", auto_error=False)
 
 
 async def verify_token(token: str = Depends(token_scheme)):
+    if not token:
+        raise HTTPException(403, {"error": "Ошибка доступа"})
 
-    invalid_msg = {"error": "Ошибка доступа"}
-    expired_msg = {"error": "Истёкшая сессия"}
-    server_error = {"error": "Server error"}
-
-    if token:
-        try:
-            data = jwt.decode(token, settings.secret_key, algorithms=["HS256"])
-            if 'sub' in data:
-                user_id = data['sub']['user_id']
-                user_login = data['sub']['user_login']
-
-        except jwt.ExpiredSignatureError:
-            code = 403
-            raise HTTPException(code, expired_msg)
-        except jwt.InvalidTokenError:
-            code = 403
-            raise HTTPException(code, invalid_msg)
-        except Exception:
-            code = 500
-            raise HTTPException(code, server_error)
-
-
+    try:
+        data = jwt.decode(token, settings.secret_key, algorithms=["HS256"])
+        user_id = data["sub"]["user_id"]
+        user_login = data["sub"]["user_login"]
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(403, {"error": "Истёкшая сессия"})
+    except (jwt.InvalidTokenError, KeyError):
+        raise HTTPException(403, {"error": "Ошибка доступа"})
+    except Exception:
+        raise HTTPException(500, {"error": "Server error"})
 
     return User(id=user_id, login=user_login)
